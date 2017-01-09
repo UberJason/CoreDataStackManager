61f7ccecfe65a4ba6c82cfe56c920e0935d42cca
Jason Ji <uberjason@gmail.com>
2017-01-08 21:37:12 -0500

    Remove example project

--- Example/Pods/Target Support Files/CoreDataStackManager/CoreDataStackManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CoreDataStackManager : NSObject
-@end
-@implementation PodsDummy_CoreDataStackManager
-@end
--- Example/Pods/Target Support Files/Pods-CoreDataStackManager_Tests/Pods-CoreDataStackManager_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_CoreDataStackManager_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CoreDataStackManager_TestsVersionString[];
-

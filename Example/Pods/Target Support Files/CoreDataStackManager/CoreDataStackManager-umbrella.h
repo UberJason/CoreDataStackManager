da3555cc16931289d259f0f92e41e6c88b65b553
Jason Ji <uberjason@gmail.com>
2017-01-08 21:50:03 -0500

    Revert "Remove example project"
    This reverts commit 61f7ccecfe65a4ba6c82cfe56c920e0935d42cca.

---
+++ Example/Pods/Target Support Files/CoreDataStackManager/CoreDataStackManager-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double CoreDataStackManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoreDataStackManagerVersionString[];
+
